569830d0e8c32853895815d01f4240b715c1a838
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2024-02-25 08:26:27 +0900

    deviseコントローラでルーティング設定およびホーム画面用Topsコントローラ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :coonfigure_permitted_params, if: :devise_controller?
+
+  def after_sign_up_path_for(resource)
+    "/users/#{current_user.id}"
+  end
+
+  protected
+
+    # 入力フォームからアカウント名をDBに保存する
+    def coonfigure_permitted_params
+      devise_paramater_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'tops/index'
+  devise_for :users, :controllers => { 
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    get "user/:id", :to => "users/registrations#detail"
+    get "signup",   :to => "users/registrations#new"
+    get "login",    :to => "users/sessions#new"
+    get "logout",   :to => "users/sessions#destroy"
+  end
 end
