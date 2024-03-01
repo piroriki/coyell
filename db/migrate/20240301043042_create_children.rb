2b80168ab39e1975bb5e4df837ec9945ab4fa86a
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2024-03-01 13:35:09 +0900

    childrenモデル作成およびER図にchildrenモデル追加

---
+++ db/migrate/20240301043042_create_children.rb
@@ -0,0 +1,12 @@
+class CreateChildren < ActiveRecord::Migration[7.0]
+  def change
+    create_table :children do |t|
+      t.string :child_name, null: false
+      t.string :gender, null: false
+      t.integer :age, null: false
+      t.date :birthday, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_22_232328) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_01_043042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "children", force: :cascade do |t|
+    t.string "child_name", null: false
+    t.string "gender", null: false
+    t.integer "age", null: false
+    t.date "birthday", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
